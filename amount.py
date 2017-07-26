454f6f063e03091ced1801d2205799edcf19afeb
Sergei <30337160+BeachbodyNumbercrunch@users.noreply.github.com>
2017-07-26 22:01:58 +0300

    Update amount.py

--- amount.py
+++ amount.py
@@ -1,7 +1,7 @@
 import pygame, os
 def amount(clicked, clicked_x, cliked_y, button, button_width, button_height):
     amount = 1
-    pygame.display.blit(button1,(1920 / 2 - button1_width / 2, 0))
+    pygame.display.blit(button,(1920 / 2 - button_width / 2, 0))
     if cliked_x < (1920 / 2 + button_width / 2) and cliked_x > (1920 / 2 - button_width / 2) and cliked_y < button_height and cliked_x > 0:
         if clicked_y < button_height / 3:
             amount = 1
