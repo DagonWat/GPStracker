cbfc5cb6ce4ee9a7041448e080f2ddac4570e630
DagonWat <voitehovskii_egor@mail.ru>
2017-09-12 21:39:25 +0300

    uploading form

---
+++ web/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+    get 'welcome/index'
+
+    resources :articles do
+        resources :comments
+
+        member do
+            get '/lol', to: 'articles#lol', via: :get
+            #match '/lol' => 'articles#lol', via: :get
+        end
+    end
+
+    root "welcome#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
