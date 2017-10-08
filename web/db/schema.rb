4875e8c74f24f6ac3231d3879a286fbbb3f59ac8
DagonWat <voitehovskii_egor@mail.ru>
2017-10-08 14:34:27 +0300

    postgres

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -12,21 +12,8 @@
 
 ActiveRecord::Schema.define(version: 20170920191707) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "trackers", force: :cascade do |t|
     t.float "lat"
