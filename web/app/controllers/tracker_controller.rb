e422b910dc1f47fb62b66a4df8209c4e8063a55b
DagonWat <voitehovskii_egor@mail.ru>
2017-10-09 22:44:36 +0300

    gitignore problem 1

--- web/app/controllers/tracker_controller.rb
+++ web/app/controllers/tracker_controller.rb
@@ -1,15 +1,15 @@
 class TrackerController < ApplicationController
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-    def create
+	def create
 
-        if params[:latitude] > 0 and params[:longitude] > 0
-            @tracker = Tracker.new(lat: params[:latitude], lon: params[:longitude])
+    if params[:latitude] > 0 and params[:longitude] > 0
+      @tracker = Tracker.new(lat: params[:latitude], lon: params[:longitude])
 
-            @tracker.save
+			@tracker.save
 
-            p params[:latitude]
-            render json: {status: "success"}
-        end
-    end
+			p params[:latitude]
+			render json: {status: "success"}
+		end
+	end
 end
