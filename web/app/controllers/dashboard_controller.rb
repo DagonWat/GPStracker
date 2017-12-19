91e23e0fbf3db477980f06994a188de8012f1d37
Alexey Ramazanov <me@sharkus.net>
2017-12-19 19:43:02 +0300

    calendar refactoring

--- web/app/controllers/dashboard_controller.rb
+++ web/app/controllers/dashboard_controller.rb
@@ -4,17 +4,15 @@ class DashboardController < ApplicationController
   before_action :check_if_admin
 
   def show
-    if params[:id].present? && Tracker.find(params[:id]).user_id == current_user.id
-      @from = Tracker.find(params[:id]).created_at.strftime('%Y-%m-%d 00:00:00')
-
-      @until = Tracker.find(params[:id]).created_at.strftime('%Y-%m-%d 23:59:59')
-
-      @trackers = Tracker.where(user_id: current_user.id).order(:created_at)
+    if params[:date].present?
+      date = Time.zone.parse(params[:date])
+      @from = date.beginning_of_day
+      @until = date.end_of_day
 
       @paths = []
       current_path = []
 
-      Tracker.order(:created_at).where(user_id: current_user.id).where('created_at >= :start_date AND created_at <= :end_date', {start_date: @from, end_date: @until}).each do |tracker|
+      current_user.trackers.order(:created_at).where('created_at BETWEEN ? AND ?', @from, @until).each do |tracker|
         if current_path.empty?
           current_path << tracker
           next
@@ -36,8 +34,44 @@ class DashboardController < ApplicationController
       @until = Time.now.strftime('%Y-%m-%d 23:59:59')
 
       @today = Tracker.where(user_id: current_user.id).where('created_at >= :start_date AND created_at <= :end_date',
-        {start_date: @from, end_date: @until})
+                                                             {start_date: @from, end_date: @until})
     end
+
+    # user_id == current_user.id
+    # if params[:id].present? && Tracker.find(params[:id]).user_id == current_user.id
+    #   @from = Tracker.find(params[:id]).created_at.strftime('%Y-%m-%d 00:00:00')
+    #
+    #   @until = Tracker.find(params[:id]).created_at.strftime('%Y-%m-%d 23:59:59')
+    #
+    #   @trackers = Tracker.where(user_id: current_user.id).order(:created_at)
+    #
+    #   @paths = []
+    #   current_path = []
+    #
+    #   Tracker.order(:created_at).where(user_id: current_user.id).where('created_at >= :start_date AND created_at <= :end_date', {start_date: @from, end_date: @until}).each do |tracker|
+    #     if current_path.empty?
+    #       current_path << tracker
+    #       next
+    #     end
+    #
+    #     if current_path.last.created_at + 15.minutes < tracker.created_at
+    #       @paths << current_path
+    #       current_path = [tracker]
+    #     else
+    #       current_path << tracker
+    #     end
+    #   end
+    #
+    #   @paths << current_path if current_path.any?
+    # else
+    #   @trackers = Tracker.where(user_id: current_user.id).order(:created_at)
+    #
+    #   @from = Time.now.strftime('%Y-%m-%d 00:00:00')
+    #   @until = Time.now.strftime('%Y-%m-%d 23:59:59')
+    #
+    #   @today = Tracker.where(user_id: current_user.id).where('created_at >= :start_date AND created_at <= :end_date',
+    #     {start_date: @from, end_date: @until})
+    # end
   end
 
   protected
--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+
+  # NOTE
+  has_many :trackers
 end
