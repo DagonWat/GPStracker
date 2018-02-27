750ab5dc94349feff5636c4aa970ee071bbe4479
DagonWat <voitehovskii_egor@mail.ru>
2018-02-27 22:58:56 +0300

    fix-7

--- web/app/mailers/user_mailer.rb
+++ web/app/mailers/user_mailer.rb
@@ -7,8 +7,7 @@ class UserMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
     @url = activate_registration_url(id: user.activation_token)
-    mail(:to => user.email,
-       :subject => "Welcome to My Awesome Site")
+    mail(to: user.email, subject: 'Welcome to tracker-dev!')
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -19,14 +18,12 @@ class UserMailer < ApplicationMailer
   def activation_success_email(user)
     @user = user
     @url  = login_url()
-    mail(:to => user.email,
-       :subject => "Your account is now activated")
+    mail(to: user.email, subject: 'Your account is now activated')
   end
 
   def reset_password_email(user)
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
-    mail(:to => user.email,
-         :subject => "Reset your password")
+    mail(to: user.email, subject: 'Reset your password')
   end
 end
