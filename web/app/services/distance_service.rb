36723a78a571b86c06d498c24a975237560ddab7
DagonWat <voitehovskii_egor@mail.ru>
2018-03-04 22:06:26 +0300

    fix-9

--- web/app/services/distance_service.rb
+++ web/app/services/distance_service.rb
@@ -7,9 +7,6 @@ class DistanceService
   end
 
   def make_good()
-    p track_new.created_at
-    p track_old.created_at
-    p track_old.created_at
     distance_x = (track_old.lat - track_new.lat) * 71.2403572324
     distance_y = (track_old.lon - track_new.lon) * 111.13486111
     distance = Math.hypot(distance_y, distance_x);
