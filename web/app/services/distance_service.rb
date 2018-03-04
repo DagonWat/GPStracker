056b25a57c009241f63d53dcfe68d3ab6a42c025
DagonWat <voitehovskii_egor@mail.ru>
2018-03-04 21:41:29 +0300

    fix-9

--- web/app/services/distance_service.rb
+++ web/app/services/distance_service.rb
@@ -7,7 +7,7 @@ class DistanceService
   end
 
   def make_good()
-    p track_old.created_at - track_old.created_at
+    p track_new.created_at - track_old.created_at
     p track_old.created_at
     p track_old.created_at
     distance_x = (track_old.lat - track_new.lat) * 71.2403572324
