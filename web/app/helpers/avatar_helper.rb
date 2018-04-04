74782fd6f80c2f2a267a7f885cdcaba3158e070b
DagonWat <voitehovskii_egor@mail.ru>
2018-04-04 07:26:09 +0300

    lil fix edit form

--- web/app/helpers/avatar_helper.rb
+++ web/app/helpers/avatar_helper.rb
@@ -2,6 +2,7 @@ module AvatarHelper
   def user_avatar_thumb(user)
     user.custom_avatar==nil ? user.avatar.thumb.url : user.custom_avatar_thumb
   end
+
   def user_avatar_medium(user)
     user.custom_avatar==nil ? user.avatar.medium.url : user.custom_avatar
   end
